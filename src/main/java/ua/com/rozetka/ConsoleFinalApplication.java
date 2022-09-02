75e755b61d261cee9bd899f5d03fa1a004ac2a17
Andrii Shylin <andrii.shylin@refinitiv.com>
2022-09-02 20:55:16 +0300

    add dev branch

--- src/main/java/ua/com/rozetka/ConsoleFinalApplication.java
+++ src/main/java/ua/com/rozetka/ConsoleFinalApplication.java
@@ -2,6 +2,8 @@ package ua.com.rozetka;
 
 public class ConsoleFinalApplication {
     public static void main(String[] args) {
+//        TODO:
         System.out.println("Start");
+        System.out.println(" add dev branch");
     }
 }
